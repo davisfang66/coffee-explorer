2bd937d9b1fe3c7ca9499e314facfe695841f26c
coffee-explorer <you@example.com>
2026-07-01 12:46:11 +0800

    fix mobile viewport

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,17 @@
 function Navbar() {
-    return (
-      <nav className="navbar">
-        <div className="logo">🍪饼干做的很辛苦的喵</div>
-  
+  return (
+    <nav className="navbar">
+      <div className="logo">
+        <span>🍪饼干做的很辛苦的喵</span>
 
-        </div>
-      </nav>
-    )
-  }
-  
-  export default Navbar
+        <img
+          className="nav-qrcode"
+          src="/222.png"
+          alt="二维码"
+        />
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar

648f05736b5ac02b023b4e56ab100ad8d6cfef6c
coffee-explorer <you@example.com>
2026-07-01 12:32:42 +0800

    fix mobile viewport

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,8 +3,7 @@ function Navbar() {
       <nav className="navbar">
         <div className="logo">🍪饼干做的很辛苦的喵</div>
   
-        <div className="nav-links">
-          <a href="#">首页</a>
+
         </div>
       </nav>
     )
